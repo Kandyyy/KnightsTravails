a18d78a6b1afa8acb65c8757ae80f2198eadf067
Kandyyy <kingkandy016@gmail.com>
2023-05-23 02:35:34 +0530

    Added MinimumMoves function

--- KnightTravails.py
+++ KnightTravails.py
@@ -1,6 +1,26 @@
+from collections import deque
+
 def MinimumMoves(start, end):
     
-    return
+    queue = deque()
+    visited_nodes = []
+    distance = {}
+    distance[start] = 0
+    
+    queue.append(start)
+    visited_nodes.append(start)
+    while queue:
+        curr_node = queue.popleft()
+        if curr_node == end:
+            return distance[curr_node]
+        possible_moves = get_valid_moves(curr=curr_node)
+        for move in possible_moves:
+            if move not in visited_nodes:
+                queue.append(move)
+                visited_nodes.append(move)
+                distance[move] = distance[curr_node] + 1
+
+    return 
 
 def get_valid_moves(curr):
     
@@ -18,13 +38,14 @@ def get_valid_moves(curr):
         [curr_x-1, curr_y-2]
     ]
 
+
     for i in range(8):
         if possible_moves[i][0] >= 0 and possible_moves[i][0] <= 7 and possible_moves[i][1] >= 0 and possible_moves[i][1] <= 7:
-            valid_moves.append([possible_moves[i][0], possible_moves[i][1]])
+            valid_moves.append((possible_moves[i][0], possible_moves[i][1]))
             
     return valid_moves
 
-start = [3,3]
-end = [1,2]
+start = (0,0)
+end = (3,1)
 
-print(get_valid_moves(start))
+print(MinimumMoves(start, end))
